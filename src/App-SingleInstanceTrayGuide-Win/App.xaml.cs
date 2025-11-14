8570a9862e76b9a116e9c8bf8f222cfab139b419
sjain882 <43217178+sjain882@users.noreply.github.com>
2025-11-14 20:58:04 +0000

    initial commit

---
+++ src/App-SingleInstanceTrayGuide-Win/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace App_SingleInstanceTrayGuide_Win;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
