edf5a73ca2a5ea682772779a2381b4cfe35f94ad
sjain882 <43217178+sjain882@users.noreply.github.com>
2025-11-15 23:46:40 +0000

    feat: GUi is loaded with config applied

--- src/App-SingleInstanceTrayGuide-Win/App.xaml.cs
+++ src/App-SingleInstanceTrayGuide-Win/App.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System.Configuration;
 using System.Data;
+using System.Diagnostics;
+using System.Runtime.InteropServices.JavaScript;
 using System.Windows;
 using App_SingleInstanceTrayGuide_Win.Config;
 
@@ -10,16 +12,43 @@ namespace App_SingleInstanceTrayGuide_Win;
 /// </summary>
 public partial class App : Application
 {
+    private TrayGuideWindow _trayGuideWindow;
+    private DebugConfig _debugConfig;
+    private AppLaunchConfig _appLaunchConfig;
+    private TrayGuideWindowConfig _trayGuideWindowConfig;
+    private ErrorMessagesConfig _errorMessagesConfig;
+    
+    
     protected override void OnStartup(StartupEventArgs e)
     {
         base.OnStartup(e);
         
         // Load config
-        var debugConfig = new DebugConfig();
-        var appLaunchConfig = new AppLaunchConfig();
-        var trayGuideWindowConfig = new TrayGuideWindowConfig();
+        _debugConfig = new DebugConfig();
+        _appLaunchConfig = new AppLaunchConfig();
+        _trayGuideWindowConfig = new TrayGuideWindowConfig();
+        _errorMessagesConfig = new ErrorMessagesConfig();
+
+        // Debug logic
+        switch (_debugConfig.ShowGUI)
+        {
+            case 0:
+                break;
+            case 1:
+                _trayGuideWindowConfig.SetIsAppAlreadyRunning(true);
+                MessageBox.Show(_trayGuideWindowConfig.TitleLabelText);
+                _trayGuideWindow = new TrayGuideWindow(_trayGuideWindowConfig);
+                _trayGuideWindow.Show();
+                break;
+            case 2:
+                _trayGuideWindowConfig.SetIsAppAlreadyRunning(false);
+                MessageBox.Show(_trayGuideWindowConfig.TitleLabelText);
+                _trayGuideWindow = new TrayGuideWindow(_trayGuideWindowConfig);
+                _trayGuideWindow.Show();
+                break;
+            default:
+                break;
+        }
         
-        var trayGuideWindow = new TrayGuideWindow();
-        trayGuideWindow.Show();
     }
 }
