5236920bd38fae7e7d0d61fec3c971a8c0f3d64a
sjain882 <43217178+sjain882@users.noreply.github.com>
2025-11-15 22:15:19 +0000

    feat: link View to ViewModel and implement OK button

---
+++ src/App-SingleInstanceTrayGuide-Win/GUI/ViewModel/Commands/CloseWindowCommand.cs
@@ -0,0 +1,23 @@
+using System.Windows;
+using System.Windows.Input;
+
+namespace App_SingleInstanceTrayGuide_Win.GUI.ViewModel.Commands;
+
+public class CloseWindowCommand : ICommand
+{
+    public bool CanExecute(object parameter)
+    {
+        return true;
+    }
+    
+    public event EventHandler CanExecuteChanged
+    {
+        add { CommandManager.RequerySuggested += value; }
+        remove { CommandManager.RequerySuggested -= value; }
+    }
+    
+    public void Execute(object? parameter)
+    {
+        Application.Current.Shutdown(0);
+    }
+}
